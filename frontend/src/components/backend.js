c845174e8c849e1769b35790b72f23df30fe32d6
vinaybarve <vinaybarve40@gmail.com>
2023-12-05 14:08:47 +0530

    adding data

--- frontend/src/components/backend.js
+++ frontend/src/components/backend.js
@@ -1,4 +1,4 @@
 // export const site = "https://adventurous-wasp-scrubs.cyclic.app"
-// export const site = "https://exuberant-clam-skirt.cyclic.app"
+export const site = "https://backend-proj-xfg9.onrender.com"
 
-export const site = "http://localhost:5501"
+// export const site = "http://localhost:5501"
